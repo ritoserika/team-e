89e977d4cc0aa9936486425641dc342d4166aea1
YutaSeri <s20b1031@bene.fit.ac.jp>
2021-10-30 17:13:35 +0900

    チャンネル名に[]をつける

--- laratter/resources/views/dashboard2.blade.php
+++ laratter/resources/views/dashboard2.blade.php
@@ -15,19 +15,19 @@ $random_number = rand(1,5);
 //乱数の値で条件分岐
 
 if($random_number==1){
-    $result='<a href="https://youtu.be/dbG7ISuUzJQ">ソロめしTV</a><div>
+    $result='<a href="https://youtu.be/dbG7ISuUzJQ">[ソロめしTV]</a><div>
     <iframe width="560" height="315" src="https://www.youtube.com/embed/dbG7ISuUzJQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
     }elseif($random_number==2){
-        $result='<a href="https://youtu.be/Z_fnt5KJtTg">まるめし</a><div>
+        $result='<a href="https://youtu.be/Z_fnt5KJtTg">[まるめし]</a><div>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/Z_fnt5KJtTg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
     }
     else if ($random_number==3){
-        $result='<a href="https://youtu.be/YjhMa72OJn8">てりやきちゃんねる</a><div>
+        $result='<a href="https://youtu.be/YjhMa72OJn8">[てりやきちゃんねる]</a><div>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/YjhMa72OJn8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
     }
     
     else if ($random_number==4){
-        $result='<a href="https://youtu.be/CLrvw8JOcU0">ごはん日記 </a><div>
+        $result='<a href="https://youtu.be/CLrvw8JOcU0">[ごはん日記]</a><div>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/CLrvw8JOcU0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
     }
     
@@ -46,7 +46,7 @@ if($random_number==1){
 </head>
   </head>
   <body>
-    <h1>今日、あなたにおすすめするのは☞<?=$result?></h1>
+    <h1>今日、あなたにおすすめするのは<?=$result?></h1>
     <p>気に入りましたか？</p>
   <x-nav-link :href="route('tweet.create')" :active="request()->routeIs('tweet.index')">
             {{ __('お気に入り登録')}}
